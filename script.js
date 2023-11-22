dd02df4ddfa7f64fbcf5dd76bc0076fed6e249f6
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-22 10:14:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+	const dial = document.querySelector("#time");
+function displayClock() {
+	let currDate = new Date();
+	dial.innerHTML = `${currDate.toLocaleDateString()}, ${currDate.toLocaleTimeString()}`;
+}
+setInterval(displayClock, 1000);
